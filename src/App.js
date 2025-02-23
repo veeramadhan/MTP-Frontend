e163f9d5595881cca399c3a32f6db532d51cf512
veeramadhan <vmathan360@gmail.com>
2025-02-23 13:22:25 +0530

    twak chat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { Navbar } from "../src/components/Header/Navbar";
 import { Home } from './components/Home';
 import { Destination } from './components/Destination';
@@ -7,6 +8,15 @@ import { Gallery } from './components/Gallery';
 import { About } from './components/About';
 
 const App = () => {
+
+  useEffect(() => {
+    var script = document.createElement('script');
+    script.src = 'https://embed.tawk.to/67bad14b899774190ded0296/1ikos34es';
+    script.async = true;
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
+    document.body.appendChild(script);
+  }, []);
   return (
    <div>
       <Navbar />
